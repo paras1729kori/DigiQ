d93ff725622a11ab0f279c4d71ebc34f78e7f3f4
paras1729kori <paras1799kori@gmail.com>
2020-12-16 18:32:03 +0530

    removed some anomalies

--- resources/views/pages/complete.blade.php
+++ resources/views/pages/complete.blade.php
@@ -3,27 +3,31 @@
 @section('content')
 <div class="mx-4">
     <h1 class="text-center">{{ $title }}</h1>
-    <table class="table table-hover text-center">
-        <thead style="background-color: #44C5EC">
-          <tr>
-            <th>Name</th>
-            <th>Application Number</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
+    @if($names > 0)
+        <table class="table table-hover text-center">
+            <thead style="background-color: #44C5EC">
             <tr>
-                @foreach ($names as $key => $row)
-                    @if($row['form_fill'] == '1' && $row['verified'] == '1' && $row['file_created'] == '1' && $row['payment'] == '1' && $row['email_create'] == '1')
-                        <td>{{ $row['name'] }}</td>
-                        <td>{{ $row['application_no']}}</td>
-                        <td>{{ $row['phone_no'] }}</td>
-                        <td>{{ $row['email'] }}</td> 
-                    @endif
-                @endforeach
+                <th>Name</th>
+                <th>Application Number</th>
+                <th>Phone Number</th>
+                <th>Email</th>
             </tr>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <tr>
+                    @foreach ($names as $key => $row)
+                        @if($row['form_fill'] == '1' && $row['verified'] == '1' && $row['file_created'] == '1' && $row['payment'] == '1' && $row['email_create'] == '1')
+                            <td>{{ $row['name'] }}</td>
+                            <td>{{ $row['application_no']}}</td>
+                            <td>{{ $row['phone_no'] }}</td>
+                            <td>{{ $row['email'] }}</td> 
+                        @endif
+                    @endforeach
+                </tr>
+            </tbody>
+        </table>
+    @else
+        <h4 class="text-center">No Applications completed yet</h4 class="text-center">
+    @endif
 </div>
 @endsection
